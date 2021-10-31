0119095f637d7da1b840ae1d55934f92414cd356
Api-te <91590982+Api-te@users.noreply.github.com>
2021-10-31 21:21:08 +0800

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -7,11 +7,11 @@ router.get('/', (req, res) => {
     res.sendFile(__path + '/views/home.html')
 })
 
-router.get('/docs', (req, res) => {
-    res.sendFile(__path + '/views/anime.html')
+router.get('/home', (req, res) => {
+    res.sendFile(__path + '/views/index.html')
 })
 
-router.get('/docs/anime', (req, res) => {
+router.get('/anime', (req, res) => {
     res.sendFile(__path + '/views/anime.html')
 })
 
@@ -19,31 +19,31 @@ router.get('/download', (req, res) => {
     res.sendFile(__path + '/views/download.html')
 })
 
-router.get('/docs/fun', (req, res) => {
+router.get('/fun', (req, res) => {
     res.sendFile(__path + '/views/fun.html')
 })
 
-router.get('/docs/info', (req, res) => {
+router.get('/info', (req, res) => {
     res.sendFile(__path + '/views/info.html')
 })
 
-router.get('/docs/media', (req, res) => {
+router.get('/media', (req, res) => {
     res.sendFile(__path + '/views/media.html')
 })
 
-router.get('/docs/other', (req, res) => {
+router.get('/other', (req, res) => {
     res.sendFile(__path + '/views/other.html')
 })
 
-router.get('/docs/photooxy', (req, res) => {
+router.get('/photooxy', (req, res) => {
     res.sendFile(__path + '/views/photooxy.html')
 })
 
-router.get('/docs/baseenc', (req, res) => {
+router.get('/baseenc', (req, res) => {
     res.sendFile(__path + '/views/base.html')
 })
 
-router.get('/docs/profil', (req, res) => {
+router.get('/profil', (req, res) => {
     res.sendFile(__path + '/views/pages-profile.html')
 })
 
